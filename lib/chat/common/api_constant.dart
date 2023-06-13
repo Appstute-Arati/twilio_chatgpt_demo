ef9680a1839a7be48e264282b3adcb2588dd8fc6
Arati P <arati.pailwan@zingworks.in>
2023-06-13 17:24:08 +0530

    minor changes

--- lib/chat/common/api_constant.dart
+++ lib/chat/common/api_constant.dart
@@ -3,6 +3,6 @@ class ApiConstants {
 
   static const String baseUrl = "https://api.openai.com/v1";
   static const String apiKey =
-      "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";
+      "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";
   static const String getModels = "/models";
 }
